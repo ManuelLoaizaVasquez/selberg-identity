7be6d65ee9021e2d669f8874bc7580d3846cfc2c
Manuel Loaiza <manuel.loaiza@pucp.edu.pe>
2023-06-21 09:16:55 -0500

    Move code to new folder and add ADF Test

--- code/main.cpp
+++ code/main.cpp
@@ -5,7 +5,7 @@
 
 int main(void) {
     long long x = 1LL;
-    for (int i = 1; i <= 8; i++) {
+    for (int i = 1; i <= 7; i++) {
         x *= 10LL;
         auto start = std::chrono::high_resolution_clock::now();
         long double k = K(x);
